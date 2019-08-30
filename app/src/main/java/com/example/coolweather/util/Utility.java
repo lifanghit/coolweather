6b268f9120f54c1da63078187d03390cb0886d38
Sarah Li <sarah.li@pb.com>
2019-08-30 17:30:24 +0800

    显示天气信息

--- app/src/main/java/com/example/coolweather/util/Utility.java
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -7,6 +7,8 @@ import android.text.TextUtils;
 import com.example.coolweather.db.City;
 import com.example.coolweather.db.Country;
 import com.example.coolweather.db.Province;
+import com.example.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -102,5 +104,30 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /***
+     *   将返回的JSON数据解析为Weather实体类
+     *   通过JSONObject和JSONArray将天气数据中的主体内容解析出来：
+     *      {
+     *        "basic":{},
+     *  *     "update":{},
+     *  *     "status":"ok",
+     *  *     "now":{},
+     *  *     "daily_forecast":[{},{},...]
+     *  *     "aqi":{},
+     *  *     "suggestion":{}
+     *      }
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
